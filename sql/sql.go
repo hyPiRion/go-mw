e0039bfc864f976c5e5501e810d088ec256a2bcc
Jean Niklas L'orange <jeannikl@hypirion.com>
2017-02-22 00:07:56 +0100

    Oh my, this was silly; tx is already context'd

--- sql/sql.go
+++ sql/sql.go
@@ -46,10 +46,10 @@ func (wp WrapParams) handle(resp *mw.Response, req *http.Request) (err error) {
 		if val.tx != nil {
 			// did we error, or did we panic?
 			if paniced || err != nil {
-				val.tx.Tx.Rollback()
+				val.tx.Rollback()
 				// TODO: Support error message handling in case rollback fails (?)
 			} else {
-				err = val.tx.Tx.Commit()
+				err = val.tx.Commit()
 			}
 		}
 	}()
@@ -64,52 +64,7 @@ type contextKey int
 type contextValue struct {
 	db     *sql.DB
 	dbopts *sql.TxOptions
-	tx     *Tx
-}
-
-// Tx is like sql.Tx, but rollbacks/commits are not provided, and the context is
-// implicitly provided in all calls. You can grab the underlying Tx if
-// necessary, but it is not recommended and it may be nil. You can also grab the
-// context and modify it if necessary (Its parent context should always be the
-// original ctx).
-type Tx struct {
-	Ctx context.Context
-	Tx  *sql.Tx
-}
-
-// Exec executes a query that doesn't return rows. For example: an INSERT and
-// UPDATE.
-func (tx *Tx) Exec(query string, args ...interface{}) (sql.Result, error) {
-	return tx.Tx.ExecContext(tx.Ctx, query, args...)
-}
-
-// Prepare creates a prepared statement for use within a transaction.
-//
-// The returned statement operates within the transaction and can no longer be
-// used once the transaction has been committed or rolled back.
-//
-// To use an existing prepared statement on this transaction, see Tx.Stmt.
-func (tx *Tx) Prepare(query string) (*sql.Stmt, error) {
-	return tx.Tx.PrepareContext(tx.Ctx, query)
-}
-
-// Query executes a query that returns rows, typically a SELECT.
-func (tx *Tx) Query(query string, args ...interface{}) (*sql.Rows, error) {
-	return tx.Tx.QueryContext(tx.Ctx, query, args...)
-}
-
-// QueryRow executes a query that is expected to return at most one row.
-// QueryRow always returns a non-nil value. Errors are deferred until Row's Scan
-// method is called.
-func (tx *Tx) QueryRow(query string, args ...interface{}) *sql.Row {
-	return tx.Tx.QueryRowContext(tx.Ctx, query, args...)
-}
-
-// Stmt returns a transaction-specific prepared statement from an existing
-// statement. This will only error if the transaction has trouble with
-// initialisation.
-func (tx *Tx) Stmt(stmt *sql.Stmt) *sql.Stmt {
-	return tx.Tx.StmtContext(tx.Ctx, stmt)
+	tx     *sql.Tx
 }
 
 // GetRawDB returns the raw database from the provided context, or nil if it
@@ -124,13 +79,13 @@ func GetRawDB(ctx context.Context, index int) (*sql.DB, error) {
 
 // GetTx returns a transaction for the provided context. Successive calls will
 // return the same transaction, unless the transaction initialisation failed.
-func GetTx(ctx context.Context) (*Tx, error) {
+func GetTx(ctx context.Context) (*sql.Tx, error) {
 	return GetIndexedTx(ctx, 0)
 }
 
 // GetIndexedTx works like GetTx, except that it provides the option to specify
 // which database to get a transaction from.
-func GetIndexedTx(ctx context.Context, index int) (*Tx, error) {
+func GetIndexedTx(ctx context.Context, index int) (*sql.Tx, error) {
 	val := ctx.Value(contextKey(index))
 	if val == nil {
 		return nil, &mw.ErrMissingContextValue{fmt.Sprintf("go-mw/sql.Tx[%d]", index)}
@@ -141,7 +96,7 @@ func GetIndexedTx(ctx context.Context, index int) (*Tx, error) {
 	}
 	tx, err := ctxval.db.BeginTx(ctx, ctxval.dbopts)
 	if err == nil {
-		ctxval.tx = &Tx{ctx, tx}
+		ctxval.tx = tx
 	}
-	return ctxval.tx, err
+	return tx, err
 }
