1fa97f086334a149e3b8cb8de80a881a6e169176
Jean Niklas L'orange <jeannikl@hypirion.com>
2017-02-27 16:50:03 +0100

    Implement json encoding/decoding

---
+++ json/decode.go
@@ -0,0 +1,72 @@
+package mwjson
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"reflect"
+
+	"github.com/hypirion/go-mw"
+)
+
+// ErrUnsupportedMediaType is returned from a JSON decoder if the Content-Type
+// provided is not equal to "application/json"
+var ErrUnsupportedMediaType error = handlerError{errors.New(`Unsupported media type: Expected "application/json"`)}
+
+var errorType = reflect.TypeOf((*error)(nil)).Elem()
+var responseType = reflect.TypeOf((*mw.Response)(nil))
+var requestType = reflect.TypeOf((*http.Request)(nil))
+
+// NewDecoder takes a function on the form `func(*mw.Response, *http.Request,
+// *T) error` and transforms it into an mw.Handler.
+func NewDecoder(fn interface{}) mw.Handler {
+	rfn := reflect.ValueOf(fn)
+	rtyp := rfn.Type()
+	if rtyp.Kind() != reflect.Func {
+		panic("Fn is not function")
+	}
+	if rtyp.NumIn() != 3 || rtyp.NumOut() != 1 {
+		panic(fmt.Sprintf("Fn must have 3 input arguments and 1 result value"))
+	}
+	if rtyp.In(0) != responseType {
+		panic("First argument in fn must be *mw.Response")
+	}
+	if rtyp.In(1) != requestType {
+		panic("Second argument in fn must be *http.Request")
+	}
+	if rtyp.Out(0) != errorType {
+		panic("Output argument from function must be error")
+	}
+	inputType := rtyp.In(2)
+	if inputType.Kind() != reflect.Ptr || inputType.Elem().Kind() != reflect.Struct {
+		panic("Third function argument must be a pointer to a struct")
+	}
+	return (&decoder{inputType, rfn}).handler
+}
+
+type decoder struct {
+	inputType reflect.Type
+	rfn       reflect.Value
+}
+
+func (dec *decoder) handler(resp *mw.Response, r *http.Request) error {
+	if r.Header.Get("Content-Type") != "application/json" {
+		resp.Body = ErrUnsupportedMediaType
+		resp.StatusCode = http.StatusUnsupportedMediaType
+		return mw.ErrHandled
+	}
+	rinput := reflect.New(dec.inputType)
+	input := rinput.Interface()
+	err := json.NewDecoder(r.Body).Decode(input)
+	if err != nil {
+		resp.Body = handlerError{errors.New("Malformed request body: " + err.Error())}
+		resp.StatusCode = http.StatusBadRequest
+		return mw.ErrHandled
+	}
+	rresp := reflect.ValueOf(resp)
+	rr := reflect.ValueOf(r)
+	fnRes := dec.rfn.Call([]reflect.Value{rresp, rr, rinput})
+	err, _ = fnRes[0].Interface().(error)
+	return err
+}
---
+++ json/encode.go
@@ -0,0 +1,45 @@
+package mwjson
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/hypirion/go-mw"
+)
+
+type handlerError struct {
+	Err error `json:"error"`
+}
+
+func (he handlerError) Error() string {
+	return he.Err.Error()
+}
+
+type encoder struct {
+	sub mw.Handler
+}
+
+func (e *encoder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Accept") != "application/json" {
+		http.Error(w, http.StatusText(http.StatusNotAcceptable), http.StatusNotAcceptable)
+		return
+	}
+	resp := mw.NewResponse(w)
+	err := e.sub(resp, r)
+	resp.Headers.Set("Content-Type", "application/json")
+	// Unhandled errors? People should really add an error transformer into a
+	// proper response. Perhaps panic is more appropriate.
+	if mw.IsErrUnhandled(err) {
+		resp.Body = handlerError{err}
+		resp.StatusCode = http.StatusInternalServerError
+	}
+	w.WriteHeader(resp.StatusCode)
+	json.NewEncoder(w).Encode(resp.Body)
+	// TODO: Hook to notify encoding failed somehow?
+}
+
+// NewEncoder takes a go-mw Handler and converts it into an http.Handler. It
+// will encode the body as JSON.
+func NewEncoder(h mw.Handler) http.Handler {
+	return &encoder{h}
+}
