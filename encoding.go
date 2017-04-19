21c597049be0758be6fdf1abba840fc45f43d16e
Jean Niklas L'orange <jeannikl@hypirion.com>
2017-04-19 11:50:56 +0200

    Implement IntoHandler

--- encoding.go
+++ encoding.go
@@ -31,3 +31,20 @@ func IntoHTTPMiddleware(m Middleware) func(http.Handler) http.Handler {
 		return &handlerAdapter{sub: m(handler)}
 	}
 }
+
+type handlerInverter struct {
+	http.Handler
+}
+
+func (hi *handlerInverter) serve(resp *Response, r *http.Request) error {
+	w := r.Context().Value(responseWriterKey(0)).(http.ResponseWriter)
+	hi.ServeHTTP(w, r)
+	return nil
+}
+
+// IntoHandler converts an HTTP handler into a go-mw Handler. This is useful
+// when you want to utilise go-mw middleware, and also want to print middleware
+// errors properly to the client.
+func IntoHandler(h http.Handler) Handler {
+	return (&handlerInverter{h}).serve
+}
